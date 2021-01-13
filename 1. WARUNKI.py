1a80d01b8e5082ab65bcf1792e78ec1a17b623fe
bubbles-beep <77100514+bubbles-beep@users.noreply.github.com>
2021-01-13 12:08:08 +0100

    Add files via upload

---
+++ 1. WARUNKI.py
@@ -0,0 +1,20 @@
+x=25
+if(x>=26):
+    print('liscie')
+elif(x>=27):
+    print('sa')
+elif(x<=25):
+    print('zielone')
+else:
+    print('czerwone')
+
+print('\n')
+
+#MODYFIKUJ, ZEBY SPRAWDZIC JAK DZIALA ;)
+
+x=100
+y=250
+if(x>=100 and y>=250):
+    print('hooray')
+
+#AND ALBO OR - WTEDY JEDEN WARUNEK MUSI BYC SPELNIONY
